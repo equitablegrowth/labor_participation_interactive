1f31db58eb24a61fc2517ca995c54899706623a8
Austin Clemens <austinc@mac.com>
2016-04-05 10:01:22 -0400

    final

--- untitled.py
+++
@@ -1,4 +0,0 @@
-# cleaning IPUMS-CPS data for visual exploration of rise of female labor force, stagnation of male/household wages
-
-# key vars:
-# HHINCOME: total household income, one yearly back to 1968
